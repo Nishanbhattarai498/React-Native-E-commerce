9fa9849c502ded542ac72133419f6ec1ca1c604d
nishan <bhattarainisham@gmail.com>
2026-01-10 10:55:25 +0545

    feat: implement user synchronization and deletion functions with Inngest

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,44 @@
 import mongoose from 'mongoose';
 
+const adressSchema=new mongoose.schema({
+    label:{
+        type:String,
+        required:true
+
+    },
+    fullName:{
+        type:String,
+        required:true
+    },
+    streetAddress:{
+        type:String,
+        required:true
+
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    state:{
+        type:String,
+        required:true
+    },
+    zipCode:{
+        type:String,
+        required:true
+    },
+    phoneNumber:{
+        type:String,
+        required:true
+    },
+    isDefault: {
+        type:boolean,
+        default:false,
+    },
+
+
+})
+
 
 const userSchema =new mongoose.schema({
     email:{
@@ -21,5 +60,16 @@ const userSchema =new mongoose.schema({
         type:String,
         unique:true,
         required:true
-    }
-})
+    },
+    addresses:[adressSchema],
+
+    wishlist:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Product"
+        }
+    ]
+},{timestamps:true}
+);
+
+export const User=mongoose.model("User",userSchema)
